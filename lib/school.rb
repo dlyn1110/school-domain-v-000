f374eafb5da42ed89203e499960f5f81083691de
Dani Lyn <dlyn1110@gmail.com>
2018-04-27 11:26:17 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,23 @@
-# code here!
+class School
+attr_reader :name , :roster
+
+def initialize(name)
+  @name = name
+  @roster = {}
+
+end
+
+def add_student(student, grade)
+if @roster.include?(grade)
+  @roste
+end
+
+def grade(grade)
+  @roster[grade]
+end
+
+
+
+
+
+end
